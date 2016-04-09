f03715cf9ff555d167b3c5dc4b963865650128e1
LuK1337 <priv.luk@gmail.com>
2016-04-09 18:44:32 +0200

    Initial commit

---
+++ api.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luk
+ * Date: 4/7/16
+ * Time: 5:05 PM
+ */
+
+$devices = array(
+    'ZE500KL' => 'http://www.asus.com/support/Download/39/1/0/20/KsY29RCpsJ2bea9O/32/',
+    'ZE500KG' => 'http://www.asus.com/support/Download/39/1/0/21/xY9Hj3GM8ZlMxOR8/32/',
+    'ZE550KL' => 'http://www.asus.com/support/Download/39/1/0/22/ZROQURlyupei1ZQz/32/',
+    'ZE551KL' => 'http://www.asus.com/support/Download/39/1/0/23/SZCx58yhB5Jst0yI/8/',
+    'ZD551KL' => 'http://www.asus.com/support/Download/39/1/0/17/a3iCfYUnl9DJAg41/32/',
+    'ZE600KL' => 'http://www.asus.com/support/Download/39/1/0/24/WnVZyhpDzHw3JqH0/32/',
+    'ZE601KL' => 'http://www.asus.com/support/Download/39/1/0/25/X3KCz1JmjdVkQtTo/32/'
+);
+
+$fw_xpath = '//*[@id="div_type_20"]//a';
+$fw_regex_region = '(WW|CN|JP|TW|RKT)';
+$fw_regex_version = '(\d+\.\d+\.\d+\.\d+)';
+
+$requests = array();
+$data = array();
+$curl = curl_multi_init();
+$running = false;
+
+foreach ($devices as $device => $url) {
+    $request = curl_init($url);
+    $requests[$device] = $request;
+    $data[$device] = array();
+
+    curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+    curl_multi_add_handle($curl, $requests[$device]);
+}
+
+do {
+    curl_multi_exec($curl, $running);
+} while ($running);
+
+foreach ($requests as $device => $request) {
+    curl_multi_remove_handle($curl, $request);
+
+    $html = new DOMDocument;
+    @$html->loadHTML(curl_multi_getcontent($request));
+    $xpath = new DOMXPath($html);
+
+    foreach ($xpath->query($fw_xpath) as $element) {
+        $url = $element->getAttribute("href");
+        $fw = array();
+
+        if (!$url)
+            continue;
+
+        $fw['url'] = $url;
+
+        preg_match($fw_regex_region, $url, $matches);
+        $fw['region'] = $matches[0];
+
+        preg_match($fw_regex_version, $url, $matches);
+        $fw['version'] = $matches[0];
+
+        array_push($data[$device], $fw);
+    }
+}
+
+curl_multi_close($curl);
+print(json_encode($data));
---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(".scrollTo").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".panel[device=" + $(this).html() + "]").offset().top - 70
+    }, 1000);
+});
+
+
+var deviceTemplate = '<div class="panel panel-default" device="{ $DEVICE }"><div class="panel-heading">{ $DEVICE }</div><table class="table"><thead><tr><th>#</th><th>Version</th><th>Region</th><th>Download link</th></tr></thead><tbody></tbody></table></div>';
+var firmwareTemplate = '<tr><th scope="row">{ $INDEX }</th><td>{ $VERSION }</td><td>{ $REGION }</td><td><a href="{ $URL }">{ $URL }</a></td>';
+
+$.getJSON('./api.php', function(data) {
+    $.each(data, function(device, firmwares) {
+        $(".container").append(deviceTemplate.replace(/{ \$DEVICE }/g, device));
+
+        $.each(firmwares, function(index, firmware) {
+$(".container > div[device=" + device + "] tbody").append(
+        firmwareTemplate.replace(/{ \$INDEX }/g, index + 1)
+    .replace(/{ \$VERSION }/g, firmware['version'])
+    .replace(/{ \$REGION }/g, firmware['region'])
+    .replace(/{ \$URL }/g, firmware['url'])
+);
+        });
+    });
+
+    $("#spinner").remove();
+});
