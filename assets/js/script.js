d31b8e496c134f8745daa6507094362be03d4020
LuK1337 <priv.luk@gmail.com>
2016-04-16 18:08:39 +0200

    Add class 'active' to the dropdown items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var deviceMenuTemplate = '<li><a href="javascript:;" class="scrollTo">{ $DEVICE }</a></li>';
-var deviceTemplate = '<div class="panel panel-default" device="{ $DEVICE }"><div class="panel-heading">{ $DEVICE }<div class="btn-group pull-right"> <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Firmware <span class="caret"></span> </button> <ul class="dropdown-menu"><li><a href="javascript:;" category="emi_and_safety">EMI and Safety</a></li><li><a href="javascript:;" category="firmware">Firmware</a></li><li><a href="javascript:;" category="usb">USB</a></li><li><a href="javascript:;" category="source_code">Source Code</a></li><li><a href="javascript:;" category="manual">Manual</a></li></ul></div></div><table class="table"><thead><tr><th>#</th><th>Version</th><th>Release date</th><th>Description</th><th>Download link</th></tr></thead><tbody></tbody></table></div>';
+var deviceTemplate = '<div class="panel panel-default" device="{ $DEVICE }"><div class="panel-heading">{ $DEVICE }<div class="btn-group pull-right"> <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Firmware <span class="caret"></span> </button> <ul class="dropdown-menu"><li><a href="javascript:;" category="emi_and_safety">EMI and Safety</a></li><li><a href="javascript:;" class="active" category="firmware">Firmware</a></li><li><a href="javascript:;" category="usb">USB</a></li><li><a href="javascript:;" category="source_code">Source Code</a></li><li><a href="javascript:;" category="manual">Manual</a></li></ul></div></div><table class="table"><thead><tr><th>#</th><th>Version</th><th>Release date</th><th>Description</th><th>Download link</th></tr></thead><tbody></tbody></table></div>';
 var firmwareTemplate = '<tr class="category_{ $CATEGORY }"><th scope="row">{ $INDEX }</th><td>{ $VERSION }</td><td>{ $RELEASE_DATE }</td><td><a href="javascript:;" device="{ $DEVICE }" descriptionID="{ $DESCRIPTION_ID }" category="{ $CATEGORY }" class="showDescription">Show description</a></td><td><a href="{ $URL }">Download</a></td>';
 var descriptions = [];
 
@@ -36,8 +36,12 @@ $.getJSON('./api.php', function(data) {
         var button = $(this).parents(".btn-group").children("button");
 
         button.html($(this).html() + " <span class=\"caret\"></span>");
+
         tbody.children("tr").css('display', 'none');
         tbody.children(".category_" + $(this).attr('category')).css('display', 'table-row');
+
+        $(this).parents('.dropdown-menu').find('a').attr('class', '');
+        $(this).attr('class', 'active');
     });
 
     $(".showDescription").click(function() {
