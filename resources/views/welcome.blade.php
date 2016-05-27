06e59f72fb52ad76e20f17e67a2b39f53b6bf37f
LuK1337 <priv.luk@gmail.com>
2016-05-27 18:45:07 +0200

    Fix welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">[ cool welcome page ]</div>
                 <div class="panel-body">
-                    <p>So, this website basically lists download links for Zenfone 2 Laser (and Zenfone 3 in the near future) models.</p>
+                    <p>So, this website basically lists download links for Zenfone 2 Laser (and Zenfone 3 in the near future).</p>
                     <p>You can switch between categories like: Firmware, Utilities and other using menu on top of the page :^)</p>
                 </div>
             </div>
