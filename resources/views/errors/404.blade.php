e5c9029f747508245c6b5394de58300b8456f45a
LuK1337 <priv.luk@gmail.com>
2016-05-27 16:40:06 +0200

    Add custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">404 - Not found</div>
+                    <div class="panel-body">
+                        The page you were looking for doesn't exist.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
