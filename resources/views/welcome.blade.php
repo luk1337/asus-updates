99813a0f58cb752255523d50961459295693b0e5
LuK1337 <priv.luk@gmail.com>
2016-05-27 18:47:40 +0200

    Add source code URL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
                 <div class="panel-body">
                     <p>So, this website basically lists download links for Zenfone 2 Laser (and Zenfone 3 in the near future).</p>
                     <p>You can switch between categories like: Firmware, Utilities and other using menu on top of the page :^)</p>
+                    <p>Source code is available on <a href="https://github.com/luk1337/asus-updates">github</a> so feel free to suggest any changes or host it yourself.</p>
                 </div>
             </div>
         </div>
