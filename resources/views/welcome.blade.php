604c4492771ac66a5052cf0bebb40f31800ca555
LuK1337 <priv.luk@gmail.com>
2016-05-27 16:30:24 +0200

    Make welcome page less a meme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,8 @@
             <div class="panel panel-default">
                 <div class="panel-heading">[ cool welcome page ]</div>
                 <div class="panel-body">
-                    :^)
+                    <p>So, this website basically lists download links for Zenfone 2 Laser (and Zenfone 3 in the near future) models.</p>
+                    <p>You can switch between categories like: Firmware, Utilities and other using menu on top of the page :^)</p>
                 </div>
             </div>
         </div>
