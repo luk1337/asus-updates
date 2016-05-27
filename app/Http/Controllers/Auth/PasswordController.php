416542982b4b0db4d7d74e93bea57e89c2fe35cd
LuK1337 <priv.luk@gmail.com>
2016-05-27 20:23:14 +0200

    Disable password resets

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -29,4 +29,14 @@ class PasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    public function showResetForm()
+    {
+        return redirect('/');
+    }
+
+    public function reset()
+    {
+
+    }
 }
