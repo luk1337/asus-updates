b65343c86e89c43362919460e9c359f297744edd
LuK1337 <priv.luk@gmail.com>
2016-07-29 17:34:47 +0200

    We actually list ZF3 now :^)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">[ cool welcome page ]</div>
                 <div class="panel-body">
-                    <p>So, this website basically lists download links for Zenfone 2 Laser (and Zenfone 3 in the near future).</p>
+                    <p>So, this website basically lists download links for Zenfone 2 Laser/Selfie and Zenfone 3.</p>
                     <p>You can switch between categories like: Firmware, Utilities and other using menu on top of the page :^)</p>
                     <p>Source code is available on <a href="https://github.com/luk1337/asus-updates">github</a> so feel free to suggest any changes or host it yourself.</p>
                 </div>
